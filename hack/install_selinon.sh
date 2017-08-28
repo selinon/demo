509da57602d0d70a9ac5af252fc96e3c54f2f66b
Fridolin Pokorny <fridolin@redhat.com>
2017-08-28 16:41:23 +0200

    Do not pin specific hashes of commits, use the latest one

--- hack/install_selinon.sh
+++ hack/install_selinon.sh
@@ -3,5 +3,5 @@ set -ex
 
 # Let's use the latest version during development
 dnf install -y git
-pip3 install -U git+https://github.com/selinon/selinonlib@0e1ca593
-pip3 install -U git+https://github.com/selinon/selinon@942a2ef
+pip3 install -U git+https://github.com/selinon/selinonlib
+pip3 install -U git+https://github.com/selinon/selinon
