16eaabd8cbcff83b07b52fc17ccfcb5573709f8b
Fridolin Pokorny <fridolin@redhat.com>
2017-12-27 11:08:26 +0100

    Selinonlib was depracated

--- hack/simulate_run.sh
+++ hack/simulate_run.sh
@@ -4,14 +4,15 @@ set -ex
 SELINON_YAML_FILES_DIR="../worker/myapp/config"
 
 which which
-which selinonlib-cli || {
-    echo "selinonlib-cli not found, please install it using install_selinon.sh script" >&2
+which selinon-cli || {
+    echo "selinon-cli not found, please install it using install_selinon.sh script" >&2
     exit 1
 }
 
-PYTHONPATH="../worker/" selinonlib-cli -vvvv simulate \
+PYTHONPATH="../worker/" monkeytype run selinon-cli -vvvv execute \
     --nodes-definition "${SELINON_YAML_FILES_DIR}/nodes.yaml" \
-    --flow-definitions "${SELINON_YAML_FILES_DIR}/flows/"*.yaml \
+    --flow-definitions "${SELINON_YAML_FILES_DIR}/flows/" \
+    --selective-task-names hello_task \
     --flow-name flow1 \
-    --node-args '{"foo": "bar"}' \
+    --node-args '{"name": "Richard Feynman"}' \
     --node-args-json
